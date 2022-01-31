274ae7b472a33cbab7b9dfc37c61a6334937f188
Jonesad-ETSU <adj.bn99@outlook.com>
2022-01-31 18:45:53 -0500

    Added wait timers for supporting large files. Added commas as folder delimiters in manual usage. Added README. Added support for using previously defined shortcuts later in the shortcuts file

--- Program.cs
+++ Program.cs
@@ -19,8 +19,7 @@ static class Program
 
 class FileManager : ApplicationContext {
     public static string? configDirectory, downloadsDirectory, userDirectory;
-    public static Dictionary<string,string> shortcuts = new Dictionary<string, string>();
-    public static Dictionary<string,string> extensions = new Dictionary<string, string>();
+    public static Dictionary<string,string> shortcuts, extensions;
     public static FileSystemWatcher? downloadsWatcher, configWatcher;
     public static Func<String, bool> isAbsolutePath = System.IO.Path.IsPathFullyQualified;
 
@@ -29,13 +28,13 @@ class FileManager : ApplicationContext {
         // Check configuration directory and read in config.
         userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         downloadsDirectory = $"{userDirectory}\\Downloads";
-        configDirectory = $@"{downloadsDirectory}\config";
+        configDirectory = $"{downloadsDirectory}\\config";
         
         useDirectory(configDirectory);
         parseConfig(configDirectory);
 
         if (File.Exists($"{configDirectory}\\EvaluateDirectoryOnStartup")) {
-            Console.WriteLine($"Run for each file in {downloadsDirectory}");
+            Console.WriteLine($"Run for each file in {downloadsDirectory} - EXCEPT NOT YET- FEATURE NOT READY YET");
             // THIS IS POTENTIALLY DANGEROUS - NEEDS TESTING FIRST
             // var files = Directory.GetFiles(downloadsDirectory);
             // foreach(var file in files) {
@@ -64,14 +63,17 @@ class FileManager : ApplicationContext {
     }
 
     public static void parseConfig(string directory) {
-        //Construct shortcuts and extension Dictionaries.
+        shortcuts = new Dictionary<string, string>();
+        extensions = new Dictionary<string, string>();
+        
         var shortsFile = useFile($"{directory}\\shorts.cfg");
         var extensionsFile = useFile($"{directory}\\extensions.cfg");
         
         readFile (
             shortsFile,
             "shorts",
-            shortcuts
+            shortcuts,
+            checkForShorts
         );
 
         readFile (
@@ -88,16 +90,17 @@ class FileManager : ApplicationContext {
     }
 
     public static void changeConfigHandler (object sender, FileSystemEventArgs e) {
-         if(configDirectory != null)
+        if(configDirectory != null)
             parseConfig(configDirectory);
     }
 
     public static void checkFile (string name = "", string fullPath = "") {        
-        Regex pullBetweenBraces = new Regex("(?<=\\[).*(?=\\])");   //Thank you Google.
+        Regex pullBetweenBraces = new Regex("^(?<=\\[).*(?=\\])");   //Thank you Google.
         Match match = pullBetweenBraces.Match(name);
         string? moveToDirectory;
         if (match.Success) {
-            var directoryStack = match.Value.Split('\\');
+            // var directoryStack = match.Value.Split('\\');
+            var directoryStack = match.Value.Split(',');
             string parent = directoryStack[0];
             string children = String.Join('\\', directoryStack.Skip(1));
             
@@ -153,23 +156,29 @@ class FileManager : ApplicationContext {
             ) {}
             to = $"{name}{counter}.{extension}";
         }
-        try {
-            File.Move(from, to);
-            Console.WriteLine($"File moved to: {to}");
-        } catch (Exception e) {
-            Console.WriteLine(e.GetBaseException());
+
+        while(!attemptMove(from, to)) { Thread.Sleep(1000); }
+        bool attemptMove(string from, string to) {
+            try {
+                File.Move(from, to);
+                Console.WriteLine($"File moved to: {to}");
+                return true;
+            } catch (Exception e) {
+                Console.WriteLine(e.GetBaseException());
+                return false;
+            }
         }
     }
 
     public static string[] checkForShorts (string[] keyVal) {
-            var elements = keyVal[1].Split('\\');
-            if (shortcuts.ContainsKey(elements[0]))
-                elements[0] = shortcuts[elements[0]];
+        var elements = keyVal[1].Split('\\');
+        if (shortcuts.ContainsKey(elements[0]))
+            elements[0] = shortcuts[elements[0]];
 
-            return new String[] {
-                keyVal[0], String.Join('\\',elements)
-            };
-        }
+        return new String[] {
+            keyVal[0], String.Join('\\',elements)
+        };
+    }
 
     public static void readFile (
         string file,
@@ -180,8 +189,11 @@ class FileManager : ApplicationContext {
         using (var sr = new StreamReader(file)) {
             while(!sr.EndOfStream) {
                 string? line = sr.ReadLine();
+                
                 if(line == null)
                     throw new Exception($"Config Line for {name} is null");
+                else
+                    line = line.Trim();
 
                 if (line.StartsWith("//")) continue; //Allow comments
 
---
+++ README.md
@@ -0,0 +1,34 @@
+### Welcome to Windows Download Sorter
+
+By default, Windows pushes every downloaded file to the User's Downloads Directory. In most situations, files can be manually redirected to a different location. However, this requires manual user intervention, and thus, is often forgotten (at least in my experience). This program aims to remedy that by providing a light-weight middleman that is configurable based on filetype.
+
+## Usage
+
+Starting the program is as simple as running the exe. To enable on startup, see [Enabling Programs on Startup in Windows 10](https://www.dell.com/support/kbdoc/en-us/000124550/how-to-add-app-to-startup-in-windows-10). By default, this program doesn't do much. Without setting a config file (see [Config](#Config)), It simply monitors for changes in the download directory. After setting up a configuration, it will automatically parse files according to the `extensions.cfg`.
+
+# Manual
+In order to manually organize a file in your downloads directory, rename it with the following syntax: `^[directory/shortcut]*`. The square brackets have to be placed at the start of the line. This also works when downloading a file from the internet; simply name it this way in the save file dialog. Moving between directories in the square brackets can be accomplished by adding a `,` between two directory names. 
+
+## Installation
+
+The Program is portable so simply download your preferred release's `.zip` file from the releases page, extract it, and run the `.exe`. 
+
+#Config
+
+Setting up configuration files should be a relatively pain-free task. Simply create a folder called `config` in your \Downloads\ directory (If you have already run the program, all necessary files should alraedy be created. These files are as follows: 
+<img align='center' src='images/needed_files.png' alt='The two needed files are extensions.cfg and shorts.cfg' />
+<br/>
+Both files follow the same, and following, grammars: 
+- `key=val`
+- `//This is a comment`
+* Comments must be at the start of the line. 
+<br/>
+However, both files serve two distinct purposes. Let's begin with an example of the `shorts.cfg` file:
+<img align='center' src='images/shortcuts.png' alt='pic=C:\Users\Jonesad\Pictures next-line doc=C:\Users\Jonesad\Documents' />
+<br/>
+
+In this file, there are two shortcuts defined. The `doc` key then references `C:\Users\Jonesad\Documents` in the rest of the program. This allows for easier specifications of heavily-nested directories. To use these shortcuts, see [manual usage](#manual), use them in the extensions definitions, or use them in future shortcuts definitions. An example extensions definition is shown below:
+<img align='center' src='images/extensions.png' alt='txt=doc next-line //rar=doc' />
+<br/>
+As you can see, this follows the same format as the `shorts.cfg` file. However, it can utilize the shortcuts defined previously. So, according to this example, any file `*.txt` that gets moved into or is created or renamed inside of the downloads Directory, will be immediately moved to the `C:\Users\Jonesad\Documents` directory. *** This can cause an issue with the default new option inside of Windows Explorer, as it creates the file, and then initializes a rename dialog. ***. <br/>
+Another interesting thing to note in the above example is that the second line begins with `//`, which signifies a comment. The way files are parsed currently does not allow for inline comments, however if the line starts with `//` it will be ignored.
